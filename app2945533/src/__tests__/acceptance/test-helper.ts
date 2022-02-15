0c5834ed447e73ba6d0920a6bc793ca8a2a1a918
MonikaDobreva <m.dobreva@student.fontys.nl>
2022-02-15 12:49:46 +0100

    cloud

---
+++ app2945533/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {App2945533Application} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new App2945533Application({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: App2945533Application;
+  client: Client;
+}
